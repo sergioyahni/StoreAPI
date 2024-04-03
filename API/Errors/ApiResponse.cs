9bdd2b5a7a02e4dd626122d93260b4728f58c55b
sergioyahni <sergioyahni@gmail.com>
2024-04-03 17:12:04 +0300

    End of Section 5

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace API.Errors;
+
+public class ApiResponse
+{
+    public ApiResponse(int statusCode, string message = null)
+    {
+        StatusCode = statusCode;
+        Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+    }
+
+    public int StatusCode { get; set; }
+    public string Message { get; set; }
+
+    private string GetDefaultMessageForStatusCode(int statusCode)
+    {
+        return statusCode switch
+        {
+            400 => "Bad Request",
+            401 => "Not Authorized",
+            404 => "Not Found",
+            500 => "Server Error",
+            _ => null
+        };
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,29 +1,27 @@
+using API.Errors;
+using API.Middleware;
 using Core;
 using Core.Interfaces;
 using Infrastructure;
 using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<StoreContext>( opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
+builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
